605d3099b3c49052185ab4a81c7e3696000ba233
kacperlechicki <klechicki@poczta.fm>
2023-10-17 10:00:46 +0200

    article styles

--- src/app/shared/components/article/article.component.ts
+++ src/app/shared/components/article/article.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { ArticleInterface } from '../../types/article.interface';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, IonicModule],
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
